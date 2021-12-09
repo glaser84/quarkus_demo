121a5e6f0a898cf7b52a83cd1c71345c8fd973a2
glaser84 <95409367+glaser84@users.noreply.github.com>
2021-12-09 10:31:42 +0100

    Update DemoResourceTest.java

--- src/test/java/at/cgsit/train/quarkus/DemoResourceTest.java
+++ src/test/java/at/cgsit/train/quarkus/DemoResourceTest.java
@@ -2,6 +2,7 @@ package at.cgsit.train.quarkus;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disable;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
